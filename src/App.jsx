4edba19d265f6d861cdd64f14b6942e513aad48f
K. Dushyant Reddy <dushireddy291@gmail.com>
2021-11-09 14:57:09 +0530

    giphy

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GiphyAPI from "./components/GiphyAPI";
+import "./App.css";
+const App = () => {
+  return (
+    <div>
+      <GiphyAPI />
+    </div>
+  );
+};
+
+export default App;
